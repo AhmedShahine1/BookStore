cd0f68a41a4ced0ce354c44f592aec7211f57f83
Ahmed <hmad32003@gmail.com>
2023-03-03 19:57:22 +0200

    Add project files.

---
+++ library/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace library.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string description { get; set; }
+        public Author Author { get; set; }
+    }
+}
---
+++ library/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MyApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services, such as MVC or Entity Framework, here
+            services.AddControllersWithViews();
+            services.AddMvc(op=>op.EnableEndpointRouting=false); 
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+            app.UseMvcWithDefaultRoute();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
