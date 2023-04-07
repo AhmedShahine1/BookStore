88402d15d7d5ab9a40d0da8b967ee47f92d576e2
AhmedShahine1 <hmad32003@gmail.com>
2023-04-07 16:07:10 +0200

    create controller and view index and view details

---
+++ library/Controllers/AuthorController.cs
@@ -0,0 +1,95 @@
+﻿using BookStore.Models.Repository;
+using library.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly IBookStoreRepositry<Author> authorRepository;
+
+        public AuthorController(IBookStoreRepositry<Author> authorRepositry)
+        {
+            this.authorRepository = authorRepositry;
+        }
+
+        // GET: AuthorController
+        public ActionResult Index()
+        {
+            var authors = authorRepository.List();
+            return View(authors);
+        }
+
+        // GET: AuthorController/Details/5
+        public ActionResult Details(int id)
+        {
+            var authors = authorRepository.Find(id);
+            return View(authors);
+        }
+
+        // GET: AuthorController/Create
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        // POST: AuthorController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AuthorController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AuthorController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AuthorController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AuthorController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- library/Startup.cs
+++ library/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using BookStore.Models.Repository;
+using library.Models;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,7 +13,14 @@ namespace MyApp
         {
             // Add services, such as MVC or Entity Framework, here
             services.AddControllersWithViews();
-            services.AddMvc(op=>op.EnableEndpointRouting=false); 
+            services.AddMvc(options =>
+        {
+            // Disable Endpoint Routing
+            options.EnableEndpointRouting = false;
+        });
+            services.AddSingleton<IBookStoreRepositry<Author>, AuthorRepository>();
+            services.AddSingleton<IBookStoreRepositry<Book>, BookRepository>();
+
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -20,16 +29,17 @@ namespace MyApp
             {
                 app.UseDeveloperExceptionPage();
             }
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
             app.UseMvcWithDefaultRoute();
-            app.UseRouting();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+            //app.UseMvcWithDefaultRoute();
+            //app.UseRouting();
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllers();
+            //});
         }
     }
 }
